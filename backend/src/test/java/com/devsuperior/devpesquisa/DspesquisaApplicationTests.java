76b5d942cef3db9282a6b30b037a7b830b477926
Davi Silva <davisilvaphoto@gmail.com>
2020-09-14 13:27:45 -0300

    Project created

---
+++ backend/src/test/java/com/devsuperior/devpesquisa/DspesquisaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.devpesquisa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DspesquisaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
