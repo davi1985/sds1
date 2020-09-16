bd5f94847b716c0249e68bfa2220714e7b66a7ca
Davi Silva <davisilvaphoto@gmail.com>
2020-09-16 16:36:28 -0300

    Home page created

---
+++ front-web/src/pages/Home/index.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { ReactComponent as ArrowIcon } from "../../assets/arrow.svg";
+import { ReactComponent as GamerImage } from "../../assets/gamer.svg";
+
+import "./styles.css";
+
+const Home = () => (
+  <div className="home-container">
+    <div className="home-text">
+      <h1 className="home-text-title">Quais jogos a galera gosta mais?</h1>
+      <h3 className="home-text-subtitle">
+        Clique no botão abaixo e saiba quais são os jogos que os gamers estão
+        escolhendo!
+      </h3>
+
+      <div className="home-actions">
+        <button className="home-btn">Quero saber quais são</button>
+        <div className="home-btn-icon">
+          <ArrowIcon />
+        </div>
+      </div>
+    </div>
+    <GamerImage className="home-image" />
+  </div>
+);
+
+export default Home;
